2b73ac98add848bec2fe7a59b6101545b5dcdd0f
Farhan Purnama Putra <farhanpurnamaputra313@gmail.com>
2022-03-16 21:56:13 +0700

    Melengkapi Fungsi Main

--- calculator.cs
+++ calculator.cs
@@ -1,7 +1,13 @@
 class Calculator
 {
-statistic void Mai(string[] args)
+statistic void Main(string[] args)
 {
+	// Melengkapi Fungsi Main
+	int a = 10;
+	int b = 6;
+	
+	Console.WriteLine("Hasil Penambahan : {0} + {1} = {2}", a, b, Penambahan(a, b));
+	Console.WriteLine("Hasil Pengurangan : {0} - {1} = {2}", a, b, Pengurangan(a, b));
 }
 // Fungsi Penambahan
 statistic int Penambahan(int a, int b){
